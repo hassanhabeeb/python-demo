722727238fb1526649e1fa111539ddbb015f2827
hassanhabeeb <47173431+hassanhabeeb@users.noreply.github.com>
2025-07-06 16:19:53 +0530

    Update ssm_loader.py

--- ssm_loader.py
+++ ssm_loader.py
@@ -1,15 +1,17 @@
 # ssm_loader.py
 import boto3
 import os
-from functools import lru_cache
 
-@lru_cache()
-def get_ssm_param(name, default=None, with_decryption=True):
-    ssm = boto3.client("ssm", region_name=os.getenv("AWS_REGION", "ap-south-1"))
-    try:
-        response = ssm.get_parameter(Name=name, WithDecryption=with_decryption)
-        return response['Parameter']['Value']
-    except ssm.exceptions.ParameterNotFound:
-        if default is not None:
-            return default
-        raise Exception(f"Missing required parameter: {name}")
+def fetch_all_ssm_parameters(prefix="/python-demo/"):
+    region = os.getenv("AWS_DEFAULT_REGION", "ap-south-1")
+    ssm = boto3.client("ssm", region_name=region)
+
+    paginator = ssm.get_paginator('get_parameters_by_path')
+    for page in paginator.paginate(Path=prefix, Recursive=True, WithDecryption=True):
+        for param in page['Parameters']:
+            key = param['Name'].split('/')[-1]
+            value = param['Value']
+            print(f'export {key}="{value}"')
+
+if __name__ == "__main__":
+    fetch_all_ssm_parameters()
