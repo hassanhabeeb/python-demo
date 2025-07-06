f327ab912b67cffa66f0324a846047d82ef6f889
hassanhabeeb <47173431+hassanhabeeb@users.noreply.github.com>
2025-07-06 16:18:05 +0530

    Update export_env.sh

--- export_env.sh
+++ export_env.sh
@@ -1,15 +1,2 @@
 #!/bin/bash
-
-python3 <<EOF
-import boto3
-import os
-
-ssm = boto3.client("ssm", region_name="ap-south-1")
-prefix = "/python-demo/"
-response = ssm.get_parameters_by_path(Path=prefix, Recursive=True, WithDecryption=True)
-for param in response["Parameters"]:
-    name = param["Name"].replace(prefix, "")
-    value = param["Value"].replace('"', '\\"')
-    print(f'export {name}="{value}"')
-EOF
-
+eval "$(python3 ssm_loader.py)"
